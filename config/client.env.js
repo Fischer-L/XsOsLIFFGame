58f25c37e48a2e59c9163b130251daa405de6890
TW_LW11171-M <tw_lw11171-m@TW-LW11171-M.local>
2018-06-12 17:11:05 +0800

    The tip for the Heroku deployment branch

--- config/client.env.js
+++ config/client.env.js
@@ -1,5 +1,5 @@
 'use strict';
-const LOCAL_DEV = true;
+const LOCAL_DEV = false;
 const SOCKET_URL = LOCAL_DEV ? "http://localhost:3000" : "https://xs-os-liff-game.herokuapp.com";
 module.exports = {
 	LOCAL_DEV, SOCKET_URL

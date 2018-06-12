2490e8a924d0bdcdc72c79fbf81ae89b57219914
TW_LW11171-M <tw_lw11171-m@TW-LW11171-M.local>
2018-06-12 15:03:09 +0800

    Setup the basic WebSocket connection

--- client/main.js
+++ client/main.js
@@ -3,6 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import io from 'socket.io-client';
+
+let socket = io('http://localhost:3000');
+socket.on('news', function (data) {
+	console.log(data);
+	socket.emit('my_event', { my: 'data' });
+});
+window._socket = socket;
 
 Vue.config.productionTip = false
 
--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const path = require("path");
 const express = require('express');
 const http = require('http');
+const socketIO = require('socket.io');
 
 const app = express();
 const httpSvr = http.Server(app);
+const io = socketIO(httpSvr);
 const paths = {
   publicDir: path.resolve('./dist'),
   indexPage: path.resolve('./dist/index.html')
@@ -14,6 +16,13 @@ app.use(express.static(paths.publicDir));
 
 app.get("/", (req, res) => {
   res.sendFile(paths.indexPage);
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my_event', function (data) {
+    console.log(data);
+  });
 });
 
 httpSvr.listen(3000, function(){

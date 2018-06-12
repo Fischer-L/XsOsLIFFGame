1d46a5d0530dc11db64fb6d91451a36762e706cb
TW_LW11171-M <tw_lw11171-m@TW-LW11171-M.local>
2018-06-12 19:24:46 +0800

    Add the deploy.sh

---
+++ build/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git checkout heroku
+git rebase master
+node build/build.js
+git add --all
+git commit -a --amend
+git push origin heroku -f
+git push heroku heroku:master -f
+heroku ps:scale web=1
+heroku open
